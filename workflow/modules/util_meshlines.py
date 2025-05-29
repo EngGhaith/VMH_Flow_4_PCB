59a2799e7aa2d1962d35a29f4df8eb1d31360878
MWit83 <volker@muehlhaus.com>
2025-05-29 15:32:53 +0200

    Added IHP copyright notes, added generic n-port simulation example

--- workflow/modules/util_meshlines.py
+++ workflow/modules/util_meshlines.py
@@ -1,4 +1,20 @@
-# -*- coding: utf-8 -*-
+########################################################################
+#
+# Copyright 2025 Volker Muehlhaus and IHP PDK Authors
+#
+# Licensed under the GNU General Public License, Version 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.gnu.org/licenses/gpl-3.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+########################################################################
 
 # create mesh lines for metals and dielectrics
 
@@ -89,7 +105,6 @@ def create_standard_xy_mesh(mesh, allpolygons, margin, antenna_margin, target_ce
     add_graded_meshlines (mesh, 'y', allpolygons.ymax, allpolygons.ymax + oversize,  1.5*target_cellsize, 1.3,  max_cellsize)    
     
     return mesh
-
 
 
 def create_xy_mesh_from_polygons (mesh, allpolygons, margin, antenna_margin, target_cellsize, max_cellsize):
